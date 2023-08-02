e2737883d98c0e9e00623e8ce1b55f70095b3963
Nathan <nathan.pringle@n-able.com>
2023-08-02 10:56:52 +0100

    Add utility methods for front end

--- SoftwareEngineering/MessageHandlerFacade.cs
+++ SoftwareEngineering/MessageHandlerFacade.cs
@@ -27,4 +27,37 @@ public class MessageHandlerFacade
 
         _jsonWriterService.WriteToJson(allMessagesDto);
     }
+
+    public List<SirDto> GetSirList()
+    {
+        var email = _routerService.GetEmailMessageProcessor();
+
+        return email.GetSirList();
+    }
+
+    public List<Tuple<string, int>> GetMentions()
+    {
+        var tweet = _routerService.GetTweetMessageProcessor();
+        var mentions = tweet.GetMentions();
+
+        List<Tuple<string, int>> sortedMentions = mentions
+            .Select(kv => new Tuple<string, int>(kv.Key, kv.Value))
+            .OrderByDescending(tuple => tuple.Item2)
+            .ToList();
+
+        return sortedMentions;
+    }
+
+    public List<Tuple<string, int>> GetHashtags()
+    {
+        var tweet = _routerService.GetTweetMessageProcessor();
+        var hashtags = tweet.GetHashtags();
+
+        List<Tuple<string, int>> sortedHashtags = hashtags
+            .Select(kv => new Tuple<string, int>(kv.Key, kv.Value))
+            .OrderByDescending(tuple => tuple.Item2)
+            .ToList();
+
+        return sortedHashtags;
+    }
 }
